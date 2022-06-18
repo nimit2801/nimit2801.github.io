5836b5cf7b6ea0df5d4dec9a3d3d3722c85959c7
nimit2801 <mnimitsavant@outlook.com>
2022-06-18 08:25:06 +0530

    vue-ts + vite

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <img class="img_" src="../assets/Cat.png" alt="A cat image">
+</template>
+
+<style>
+  .img_{
+    margin-top: 10em;
+  }
+</style>
