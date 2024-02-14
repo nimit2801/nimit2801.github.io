eff22f25a1745d095c278b88a3de2d8cfc2a4a21
nimit2801 <mnimitsavant@outlook.com>
2024-02-15 03:34:44 +0530

    fix analytics

--- src/main.js
+++ src/main.js
@@ -4,13 +4,6 @@ import plausible from './utils/plausible';
 import App from './App.vue';
 
 const app = createApp(App);
-// app.use(SimpleAnalytics, { skip: process.env.NODE_ENV !== 'production' });
-// app.use(VuePlausible, {
-//     domain: 'nimitsavant.me',
-//     enableAutoPageviews: true,
-//     enableAutoOutboundTracking: true,
-//     hashMode: true,
-//     apiHost: 'https://ana.idta.in/stats',
-// })
+app.use(SimpleAnalytics, { skip: process.env.NODE_ENV !== 'production' });
 app.use(plausible);
 app.mount('#app');
