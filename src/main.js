3db0e895724d26fbe7e0dc8e34659e64d503733e
nimit2801 <mnimitsavant@outlook.com>
2024-02-15 00:51:51 +0530

    Init: Plausible with configs

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import SimpleAnalytics from 'simple-analytics-vue';
 import { createApp } from 'vue';
+import { VuePlausible } from 'vue-plausible';
 import App from './App.vue';
 
 const app = createApp(App);
 app.use(SimpleAnalytics, { skip: process.env.NODE_ENV !== 'production' });
+app.use(VuePlausible, {
+    domain: 'nimitsavant.me',
+    enableAutoPageviews: true,
+    enableAutoOutboundTracking: true,
+    hashMode: true,
+    apiHost: 'https://ana.idta.in/stats',
+})
 app.mount('#app');
