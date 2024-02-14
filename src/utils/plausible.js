712ced8bfcc79ab49861ffab849092e6b7b1fd30
nimit2801 <mnimitsavant@outlook.com>
2024-02-15 03:02:21 +0530

    re-enable simple analytics and add v-plausible library

--- src/utils/plausible.js
+++ src/utils/plausible.js
@@ -3,14 +3,14 @@ import { createPlausible } from 'v-plausible/vue'
 const plausible = createPlausible({
   init: {
     domain: 'nimitsavant.me',
-    apiHost: 'https://ana.idta.in/stats',
+    apiHost: 'https://ana.idta.in',
     trackLocalhost: true,
   },
   settings: {
     enableAutoOutboundTracking: true,
     enableAutoPageviews: true,
   },
-  partytown: false,
+  partytown: true,
 })
 
 export default plausible;
