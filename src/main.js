6464681f63edbf6c3bb52c4b7528d22d11e4337e
Nimit Savant <mnimitsavant@outlook.com>
2025-12-22 03:45:35 +0530

    chore: Remove Plausible analytics and update PostHog configuration.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
 import SimpleAnalytics from "simple-analytics-vue";
 import { createApp } from "vue";
-import plausible from "./utils/plausible";
 import App from "./App.vue";
 import posthogPlugin from "./plugin/posthog.js";
 import router from "./router/index.ts";
 
 const app = createApp(App);
 app.use(SimpleAnalytics, { skip: process.env.NODE_ENV !== "production" });
-app.use(plausible);
 app.use(router);
 app.use(posthogPlugin);
 app.mount("#app");
--- src/plugin/posthog.js
+++ src/plugin/posthog.js
@@ -5,8 +5,14 @@ export default {
   install(app) {
     posthog.init('phc_gsKb3sevQd49YlPYyd1S0gZKHZIYG7WqXJAPaHBJviK', {
       api_host: 'https://eu.i.posthog.com',
-      defaults: '2025-11-30',
-      person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+      person_profiles: 'identified_only',
+      capture_pageview: false, // We handle pageviews manually in the router
+      debug: true, // Enable debug mode to see events in console
+      loaded: () => {
+        if (import.meta.env.DEV) {
+          console.log('PostHog loaded successfully in dev mode')
+        }
+      }
     })
 
     app.config.globalProperties.$posthog = posthog
